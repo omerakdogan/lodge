45ddec237d70e0b9ad6ddc286848b9d2b75efc42
Omer Akdogan <omerakdogan@icloud.com>
2022-03-26 13:19:29 -0700

    basename update

--- src/globals/globals.js
+++ src/globals/globals.js
@@ -1,5 +1,5 @@
 export const API_KEY = '08ce2d38fc067bdbf8a30dc668b93e0f';
 export const API_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwOGNlMmQzOGZjMDY3YmRiZjhhMzBkYzY2OGI5M2UwZiIsInN1YiI6IjYxZWRmZGJjM2ZhYmEwMDA2ZGM1ZTcwOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.cG5djL5pUSqpt3_FvON1HzcJ8iDM6j54gibCZhQdJU8';
-export const APP_FOLDER_NAME = 'lodge-movie';
+export const APP_FOLDER_NAME = '/';
 export const appTitle = 'Lodge Movie';
 export const appStorageName = 'movie-app-favs';
